16b8f5a44f57a91b44395b2235803d01ff14866e
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-11 15:27:57 +0200

    Add validation

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   messages = {
     username_incorrect: 'More than 10 characters and no spaces',
     email_incorrect: 'Lack of @ in email',
-    password_incorrect: 'More than 8 characters',
+    password_incorrect: '8 characters',
     accept_incorrect: 'Please confirm the agreement',
   }
 
@@ -30,7 +30,7 @@ class App extends Component {
 
     const name = e.target.name;
     const type = e.target.type;
-    console.log(this.state.accept);
+    //console.log(this.state.accept);
     if(type === 'text' || type === 'password' || type === 'email'){
       const value = e.target.value;
       this.setState({
@@ -47,7 +47,64 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log('it works');
+    const validation = this.formValidation()  
+    if(validation.coorect) {
+      this.setState({
+        username: '',
+        email: '',
+        pass: '',
+        accept: false,
+
+        errors: {
+          username: false,
+          email: false,
+          pass: false,
+          accept: false,
+        }
+      }) 
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          pass: !validation.password,
+          accept: !validation.accept,
+        }
+      })
+    }
+  }
+
+  formValidation = () => {
+    // true - ok
+    // false - not ok
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+
+    if(this.state.username.length > 10 && this.state.username.indexOf(' ') === -1) {
+      username = true;
+    }
+    if(this.state.email.indexOf('@') !== -1) {
+      email = true;
+    }
+    if(this.state.pass.length === 8) {
+      password = true;
+    }
+    if (this.state.accept) {
+      accept = true
+    }
+    if (username && email && password && accept) {
+      correct = true
+    }
+    return({
+      correct,
+      username,
+      email,
+      password,
+      accept
+    })
   }
 
   render() {
